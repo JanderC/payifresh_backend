88073da59007c14e9c6944f9f727d5a7323a8ad4
JanderC <janderalexisc@gmail.com>
2026-03-17 16:31:14 -0400

    cambios

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const pool = new Pool({
   host: process.env.DB_HOST || '200.40.68.122',
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'respaldo2',
+  database: process.env.DB_NAME || 'redmine2',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'p4ng34t3ch',
   max: 20, // Máximo de conexiones en el pool
