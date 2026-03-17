505e3722c7512890fc8aa6562a754890879980d8
JanderC <janderalexisc@gmail.com>
2026-03-17 16:21:51 -0400

    cambios

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(helmet({
 // CORS
 app.use(cors({
   
-  origin: process.env.FRONTEND_URL || 'https://front-m-helados.vercel.app',
+  origin: process.env.FRONTEND_URL || 'https://payifresh.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
